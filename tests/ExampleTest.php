1de89cc225a37d2ef4e03854e433ffe97fc943d9
uchup07 <uchup07@users.noreply.github.com>
2024-11-01 10:25:30 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,39 +6,38 @@ it('can test', function () {
     expect(true)->toBeTrue();
 });
 
-
-it('can create meeting', function() {
+it('can create meeting', function () {
     //$zoom = new LaravelZoom();
 
     $data = [
         'topic' => 'Title Topic 3',
-        'default_password'=>false,
+        'default_password' => false,
         'start_time' => '2024-11-03T08:06:01Z',
         'duration' => 60,
         'timezone' => 'Asia/Jakarta',
-        "type"=> 2,
+        'type' => 2,
         'settings' => [
             'host_video' => true,
             'participant_video' => true,
             'audio' => true,
             'approval_type' => 2,
             'waiting_room' => true,
-            'join_before_host' => true
+            'join_before_host' => true,
         ],
     ];
 
-    $meeting = LaravelZoom::createMeeting('gatot.sulistiyo@erajaya.com',$data);
+    $meeting = LaravelZoom::createMeeting('gatot.sulistiyo@erajaya.com', $data);
 
     expect($meeting)->toMatchSnapshot();
 });
 
-it('can remove meeting', function() {
-   //$zoom = new LaravelZoom();
+it('can remove meeting', function () {
+    //$zoom = new LaravelZoom();
 
-   $meeting = LaravelZoom::deleteMeeting('95952338096');
+    $meeting = LaravelZoom::deleteMeeting('95952338096');
 
-   expect($meeting)->toMatchArray([
-       'status' => true,
-       'message' => 'Meeting Deleted Successfully',
-   ]);
+    expect($meeting)->toMatchArray([
+        'status' => true,
+        'message' => 'Meeting Deleted Successfully',
+    ]);
 });
